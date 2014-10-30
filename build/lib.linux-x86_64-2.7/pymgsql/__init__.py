1213ef762545dd52ece490f8ed6c63c1f3f6b4dd
iinitz <hanamiza555@gmail.com>
2014-10-30 13:49:33 +0700

    remove build

--- build/lib.linux-x86_64-2.7/pymgsql/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-import sys
-###==========Encoding fix==========###
-reload(sys)
-sys.setdefaultencoding('UTF8')
-
-from sqlobject import *
-from .pymgsql import Schema,Resource,MongoSQL
